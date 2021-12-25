61fec32274f7934719b7e57a23fba9b3a586dd74
Éber Mainardes <mainardes.eber@gmail.com>
2021-12-25 18:06:32 -0300

    🏗️ Making AudiobookCreator.getAudiobookChapters use a more generic type

--- src/main/java/io/hexagon/auricular/base/AudiobookCreator.java
+++ src/main/java/io/hexagon/auricular/base/AudiobookCreator.java
@@ -5,12 +5,12 @@ import java.util.List;
 import io.hexagon.auricular.base.model.Audiobook;
 import io.hexagon.auricular.base.model.Chapter;
 
-public interface AudiobookCreator<T extends Audiobook> {
+public interface AudiobookCreator<T extends Audiobook, C extends Chapter> {
 
 	List<T> getAvailableAudiobooks(int limit, int offset, String title, String genre, String authorLastName);
 	
 	T getAudiobook(int id);
 	
-	List<Chapter> getAudiobookChapters(int audiobookId);
+	List<C> getAudiobookChapters(int audiobookId);
 	
 }
